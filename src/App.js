b8d11aaa5d5747abb2855a4d0a7e380b191d38ca
loudcrab <50346926+loudcrab@users.noreply.github.com>
2021-10-01 16:19:32 +0300

    zxc zxc

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import "./styles.css";
 
 export default function App() {
   const handleScroll = (event) => {
-    const elite = document.getElementById("elite").getBoundingClientRect().y;
-    const offsetWidth = document.getElementById("elite").offsetWidth;
-    if (elite < window.pageYOffset) {
-      const calc = elite - window.pageYOffset;
+    const elite = document.getElementById("elite");
+    const wftWidth = document.getElementById("wtf").offsetWidth;
+    // console.log("wftHeight", wftHeight);
 
+    const top = elite.offsetTop;
+
+    const bottom = top + elite.offsetHeight - window.innerHeight;
+    const eliteHeight = document.getElementById("elite").offsetHeight;
+
+    if (top < window.pageYOffset && bottom > window.pageYOffset) {
+      const max = eliteHeight - window.innerHeight;
+      const calc = (top - window.pageYOffset) * -1;
+      const res = (calc / max) * (wftWidth - window.innerWidth);
+
+      // console.log("wftHeight", wftHeight - window.innerHeight, res);
       document.getElementById("wtf").style.transform =
-        "translateX(" + calc / 5 + "px)";
-      // console.log('kekw')
-      // console.log("asd", document.getElementById("wtf").style.transform);
-      // const transform = document
-      //   .getElementById("wtf")
-      //   .style.transform.replace(/^\D+/g, "")
-      //   .slice(0, -2);
-      // console.log("tra", transform);
-      // document.getElementById(
-      //   "wtf"
-      // ).style.transform = `translateX(${new Date().getTime()}%);`;
-      // console.log("result", document.getElementById("wtf").style.transform);
+        "translateX(-" + res + "px)";
     }
   };
 
@@ -31,30 +30,27 @@ export default function App() {
 
   return (
     <div className="App">
-      <div className="kekw one"></div>
+      <div className="kekw"></div>
       <div className="horizontal-trigger"></div>
       <div className="horizontal-section" id="elite">
         <div className="sticky">
           <div className="omega" id="wtf">
-            <div className="horizontal-item">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Image.jpg" />
+            <div className="horizontal-item one">
+              Мы любители пива
+              <img src="https://upload.wikimedia.org/wikipedia/commons/3/3e/Weizenbier.jpg" />
             </div>
-            <div className="horizontal-item">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Image.jpg" />
+            <div className="horizontal-item one">
+              Приглашаем вас посетить Октоберфест
+              <img src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Oktoberfest1.jpg" />
             </div>
-            <div className="horizontal-item">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Image.jpg" />
-            </div>
-            <div className="horizontal-item">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Image.jpg" />
-            </div>
-            <div className="horizontal-item">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Image.jpg" />
+            <div className="horizontal-item one">
+              До встречи в октябре
+              <img src="https://www.m24.ru/b/d/nBkSUhL2hFQkkMmzJ76BvMKnxdDs95C-miTNnuWR9mOBdDebBizCnTY8qdJf6ReJ58vU9meMMok3Ee2nhSR6ISeO9G1N_wjJ=sAuHCl5wAysF1g3TUgIwLQ.jpg" />
             </div>
           </div>
         </div>
       </div>
-      <div className="kekw two"></div>
+      <div className="kekw twoo"></div>
     </div>
   );
 }
